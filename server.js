d1df21f37e3843fb87e8d73579f74e60bd16b483
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-05 13:14:54 -0500

    Updated MongoDB connection string for improved security and flexibility.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Connection to MongoDB 
-mongoose.connect('mongodb://localhost:27017/augmented-archeology', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/augmented-archeology', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB verbunden.'))
   .catch(err => console.log(err));
 
