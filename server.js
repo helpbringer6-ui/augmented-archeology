9f22f59c8f0ffda3c4338fd396f916cea6d47e6f
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-05 13:13:53 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Verbindung zur MongoDB 
+mongoose.connect('mongodb://localhost:27017/augmented-archeology', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB verbunden.'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Willkommen bei Augmented Archeology API!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
