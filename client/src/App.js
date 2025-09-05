dfe22ea3d9e51877a34dd4223ce1b7cd19d8a3cf
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-05 13:13:56 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Augmented Archeology</h1>
+      <p>Entdecken Sie historische Artefakte in Augmented Reality!</p>
+    </div>
+  );
+}
+
+export default App;
